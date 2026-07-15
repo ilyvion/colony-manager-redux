c44f509847975218b01d1a4625661b942b1be901
Alexander Krivács Schrøder <alexschrod@gmail.com>
2026-07-15 18:41:56 +0200

    fix: restore dropped AreaManager.NotifyEveryoneAreaRemoved postfix

--- Source/ColonyManagerRedux/Patches/Verse_AreaManager_NotifyEveryoneAreaRemoved.cs
+++ Source/ColonyManagerRedux/Patches/Verse_AreaManager_NotifyEveryoneAreaRemoved.cs
@@ -4,4 +4,17 @@
 namespace ColonyManagerRedux;
 
 [HarmonyPatch(typeof(AreaManager), "NotifyEveryoneAreaRemoved")]
-internal static class Verse_AreaManager_NotifyEveryoneAreaRemoved { }
+internal static class Verse_AreaManager_NotifyEveryoneAreaRemoved
+{
+    private static void Postfix(Area area)
+    {
+        if (area.Map == null)
+        {
+            return;
+        }
+        foreach (var job in Manager.For(area.Map).JobTracker.Jobs)
+        {
+            job.Notify_AreaRemoved(area);
+        }
+    }
+}
